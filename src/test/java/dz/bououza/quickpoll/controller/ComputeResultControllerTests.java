75876c7804fadb6c85fa94a3e8fb73238102da9a
fayca121 <bz.faycal@gmail.com>
2023-02-05 15:04:34 +0100

    add compute controller test

--- src/test/java/dz/bououza/quickpoll/controller/ComputeResultControllerTests.java
+++ src/test/java/dz/bououza/quickpoll/controller/ComputeResultControllerTests.java
@@ -1,2 +1,53 @@
-package dz.bououza.quickpoll.controller;public class ComputeResultControllerTests {
+package dz.bououza.quickpoll.controller;
+
+import dz.bououza.quickpoll.dto.OptionCount;
+import dz.bououza.quickpoll.dto.VoteResult;
+import dz.bououza.quickpoll.service.ComputeResultService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ComputeResultController.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class ComputeResultControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ComputeResultService computeResultService;
+
+    @Test
+    public void givenPollId_whenComputeResult_thenVoteResult() throws Exception {
+        //given
+        Collection<OptionCount> optionCounts =
+                Arrays.asList(new OptionCount(100L,2),new OptionCount(101L,1));
+        VoteResult voteResult = new VoteResult(3,optionCounts);
+
+        //when
+        given(computeResultService.computeResult(100L)).willReturn(voteResult);
+
+        ResultActions response = mockMvc.perform(get("/computeresult").param("pollId","100"));
+        //then
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.totalVotes",is(voteResult.totalVotes())))
+                .andExpect(jsonPath("$.results.size()",is(optionCounts.size())));
+    }
+
+
 }
